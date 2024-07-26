b1e0ddfc8feb693aff880dd49699a3c6e1ccd2e6
Micky Reed <xcal18r@gmail.com>
2024-07-26 15:25:18 +0800

    refactor: buttons with shared state

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
+
+import { useState } from "react";
+
 import Button from "@/components/button";
 
-function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-4">
-      <h1>React Counter Button</h1>
-      <Button />
-    </main>
-  );
+export default function Home() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+            <h1>React Counter Button</h1>
+            <Button count={count} onClick={handleClick} />
+            <Button count={count} onClick={handleClick} />
+        </main>
+    );
 }
 
-export default Home;
 
